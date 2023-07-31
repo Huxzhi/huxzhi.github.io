ec67bba7ec91bfddc010b7e984d329da0cb5e580
Huxzhi <huxzhi@gmail.com>
2023-07-31 13:58:34 +0000

    Deploying to gh-pages from @ Huxzhi/huxzhi.github.io@47987e4ec5ff391e8262cdd300cfe304be824c03 🚀

---
+++ assets/202305231113仿md渲染显示——停止过度设计你的CSS.html-542209b0.js
@@ -0,0 +1,23 @@
+import{_ as c,X as p,Y as o,Z as n,$ as s,a1 as a,a3 as l,a2 as u,G as e}from"./framework-c2b0d87a.js";const d={},r=n("h1",{id:"仿md渲染显示——停止过度设计你的css",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#仿md渲染显示——停止过度设计你的css","aria-hidden":"true"},"#"),s(" 仿md渲染显示——停止过度设计你的CSS")],-1),v={href:"https://www.bilibili.com/video/BV1Mo4y1w7XH/",target:"_blank",rel:"noopener noreferrer"},m=n("p",null,"一个简单的 css。",-1),k=u(`<p>style.css</p><div class="language-css line-numbers-mode" data-ext="css"><pre class="language-css"><code><span class="token selector">html</span> <span class="token punctuation">{</span>
+  <span class="token property">color-scheme</span><span class="token punctuation">:</span> light dark<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token selector">body</span> <span class="token punctuation">{</span>
+  <span class="token property">font-family</span><span class="token punctuation">:</span> system-ui<span class="token punctuation">;</span>
+  <span class="token property">font-size</span><span class="token punctuation">:</span> 1.125rem<span class="token punctuation">;</span>
+  <span class="token property">line-height</span><span class="token punctuation">:</span> 1.5<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token selector">main</span> <span class="token punctuation">{</span>
+  <span class="token property">width</span><span class="token punctuation">:</span> <span class="token function">min</span><span class="token punctuation">(</span>70ch<span class="token punctuation">,</span> 100% - 4rem<span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <span class="token property">margin-inline</span><span class="token punctuation">:</span> auto<span class="token punctuation">;</span>
+
+<span class="token punctuation">}</span>
+
+<span class="token selector">img,
+svg,
+video</span> <span class="token punctuation">{</span>
+  <span class="token property">max-width</span><span class="token punctuation">:</span> 100%<span class="token punctuation">;</span>
+  <span class="token property">display</span><span class="token punctuation">:</span> block<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function _(b,h){const t=e("ExternalLinkIcon"),i=e("font");return p(),o("div",null,[r,n("p",null,[n("a",v,[s("https://www.bilibili.com/video/BV1Mo4y1w7XH/"),a(t)])]),m,a(i,{color:"#92d050"},{default:l(()=>[s("仿 md 渲染显示")]),_:1}),k])}const y=c(d,[["render",_],["__file","202305231113仿md渲染显示——停止过度设计你的CSS.html.vue"]]);export{y as default};
